60132f98102e58abc99c26bb905be0af8d9abdd9
vighnesh153 <vighnesh.raut13@gmail.com>
2019-03-26 20:49:23 +0530

    init file or the seeder

---
+++ src/main.py
@@ -0,0 +1,57 @@
+# summarize functions definition in src/summarize
+from src.summarize import summarize_data_from_url, summarize
+
+
+def log_summary_for_url(fetch_url):
+    """
+    TO log the summary of an article to be extracted from an URL.
+    JUST FOR DEBUGGING
+
+    :param fetch_url:
+    :return: Nothing or None
+    """
+
+    # fetch the summary
+    summary = summarize_data_from_url(fetch_url)
+
+    # if summary is None, then it means article couldn't be fetched. Hence,
+    # log the error and Halt.
+    if summary is None:
+        print("Program halted: no article found!")
+        exit(0)
+
+    # else, Log the summary to the console
+    else:
+        print(summary)
+        return None
+
+
+def log_summary(input_title, input_text):
+    """
+    TO log the summary of an article.
+    JUST FOR DEBUGGING
+
+    :param input_title: title of the article
+    :param input_text: contents of the article
+    :return: Nothing or None
+    """
+
+    # fetch the summary
+    summary = summarize(input_title, input_text)
+
+    # if summary is None, then it means article couldn't be fetched. Hence,
+    # log the error and Halt.
+    if summary is None:
+        print("Program halted: no article found!")
+        exit(0)
+
+    # else, Log the summary to the console
+    else:
+        print(summary)
+        return None
+
+
+if __name__ == '__main__':
+
+    url = "https://www.bbc.com/news/world-asia-india-47499917"
+    log_summary_for_url(url)
