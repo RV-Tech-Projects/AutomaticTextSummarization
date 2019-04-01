d6d6bcccb597800cddf2f331f5d0c43e849fe752
vighnesh153 <vighnesh.raut13@gmail.com>
2019-04-01 22:49:25 +0530

    this is never gonna end

--- test.py
+++ test.py
@@ -1,18 +1,1 @@
-from collections import Counter
-
-
-obj = Counter()
-
-obj["a"] = 1
-obj["b"] = 343
-obj["c"] = 132
-obj["d"] = 423
-obj["e"] = 21
-obj["f"] = 25
-
-print(obj)
-for key in obj:
-    print(key)
-
-print("*" * 40)
-print(obj.most_common(3))
+print("lol")
